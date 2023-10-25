225e2b2f69ac6b1573cb2f11937a7b527299b0ec
Vinay B <vinay.bayyapunedi@gmail.com>
2023-10-25 16:18:03 +1100

    Create a ViewModifier called ThemeModifier that can be used to change the theme of the application.

--- IOS_Advanced_Assignment_4/View Model/ThemeModifier.swift
+++ IOS_Advanced_Assignment_4/View Model/ThemeModifier.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+import SwiftUI
+
+// This is a viewModifier that sets the theme of the app based on the value of the Published property "selectedTheme" from the ModelData class. The value of :selectedTheme" is defined by the UserDefaults value of the key "SelectedTheme".
+struct ThemeModifier: ViewModifier {
+    
+    // The current color scheme environment value.
+    @Environment(\.colorScheme) var colorScheme
+    
+    // The selected theme from the ModelData class
+    var selectedTheme: ModelData.AppTheme
+    
+    // If the selected theme is dark, it sets the color scheme to dark. Otherwise, it sets it to light.
+    func body(content: Content) -> some View {
+        content.preferredColorScheme(selectedTheme == .dark ? .dark : .light)
+    }
+}
