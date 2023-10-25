0c4abc9de05eb602c160174818c26302a74cd4d2
Vinay B <vinay.bayyapunedi@gmail.com>
2023-10-25 14:39:58 +1100

    Initial Commit

---
+++ IOS_Advanced_Assignment_4/IOS_Advanced_Assignment_4App.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_Advanced_Assignment_4App.swift
+//  IOS_Advanced_Assignment_4
+//
+//  Created by vinay bayyapunedi on 25/10/23.
+//
+
+import SwiftUI
+
+@main
+struct IOS_Advanced_Assignment_4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
