b97b60638246f002d4ef8470c2ba49a16f4808da
Vinay B <vinay.bayyapunedi@gmail.com>
2023-10-25 16:43:29 +1100

    Created a file structure for CoreData,

---
+++ IOS_Advanced_Assignment_4/CoreData/PersistenceController.swift
@@ -0,0 +1,16 @@
+//
+//  PersistenceController.swift
+//  IOS_Advanced_Assignment_4
+//
+//  Created by vinay bayyapunedi on 25/10/23.
+//
+
+import CoreData // Import the CoreData framework
+import Foundation
+
+// This struct manages the persistent data storage
+struct PersistenceController {
+    
+
+    
+}
