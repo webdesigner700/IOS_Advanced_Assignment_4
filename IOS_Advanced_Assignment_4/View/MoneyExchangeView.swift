bfcec48ebd0fe29639de89d08ac4d8670da06c3d
solgrace <solibungrace@gmail.com>
2023-10-27 01:50:07 +1100

    Implemented 'MoneyExchangeView' for Currency Exchange Navigation

---
+++ IOS_Advanced_Assignment_4/View/MoneyExchangeView.swift
@@ -0,0 +1,20 @@
+//
+//  MoneyExchangeView.swift
+//  IOS_Advanced_Assignment_4
+//
+//  Created by Grace Rufina Solibun on 27/10/2023.
+//
+
+import SwiftUI
+
+struct MoneyExchangeView: View {
+    var body: some View {
+        Text("MoneyExchangeView")
+    }
+}
+
+//struct MoneyExchangeView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        MoneyExchangeView()
+//    }
+//}
