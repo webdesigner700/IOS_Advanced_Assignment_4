67233b269f309bf97fefcd7e706fc5c048932cb1
solgrace <solibungrace@gmail.com>
2023-10-26 23:26:48 +1100

    Successfully defined the struct of the exchange rates data to match the exact data format received from the API.

--- IOS_Advanced_Assignment_4/View Model/ExchangeRatesResponse.swift
+++ IOS_Advanced_Assignment_4/View Model/ExchangeRatesResponse.swift
@@ -6,3 +6,11 @@
 //
 
 import Foundation
+
+struct ExchangeRatesResponse: Codable {
+    let success: Bool
+    let timestamp: Int
+    let base: String
+    let date: String
+    let rates: [String: Double]
+}
