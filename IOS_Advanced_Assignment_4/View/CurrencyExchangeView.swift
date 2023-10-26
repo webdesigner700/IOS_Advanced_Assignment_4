6b6487e9fc3517280895acf10df7b05d8051f3dc
solgrace <solibungrace@gmail.com>
2023-10-27 02:17:49 +1100

    Updated CurrencyExchangeView to hide the default '>' symbol that comes with using 'NavigationLinks' by using an overlay. Overlay helps me include extra elements, such as placing a NavigationLink that's not visible on the screen by pairing it with an EmptyView, allowing me to control navigation without a visible '>'.

--- IOS_Advanced_Assignment_4/View/CurrencyExchangeView.swift
+++ IOS_Advanced_Assignment_4/View/CurrencyExchangeView.swift
@@ -14,57 +14,67 @@ struct CurrencyExchangeView: View {
         NavigationView {
             List {
                 ForEach(viewModel.exchangeRates.rates.sorted(by: <), id: \.key) { currencyCode, exchangeRate in
-                    NavigationLink(destination: MoneyExchangeView(currencyCode: currencyCode, exchangeRate: exchangeRate)) {
-                        HStack {
-                            Spacer().frame(width: 15)
-                            
-                            Text("\(currencyCode)")
-                            
-                            Spacer().frame(width: 20)
-                            
-                            VStack {
-                                Text("Current rate:")
-                                    .frame(maxWidth: .infinity, alignment: .trailing)
-                                    .foregroundColor(.gray)
-                                    .italic()
-                                    .padding(.top, 30)
-                                Text("\(exchangeRate, specifier: "%.2f")")
-                                    .frame(maxWidth: .infinity, alignment: .trailing)
-                                    .padding(.bottom, 30)
-                            }
-                            
-                            Spacer().frame(width: 15)
-                            
-                            Divider()
-                                .frame(height: 50)
-                                .background(Color.gray)
-                            
-                            Spacer().frame(width: 20)
-                            
-                            Image(systemName: "chevron.right")
-                                .padding(.trailing, 10)
-                                .foregroundColor(.black)
-                            
-                            Spacer().frame(width: 1)
+                    HStack {
+                        Spacer().frame(width: 15)
+                        
+                        Text("\(currencyCode)")
+                        
+                        Spacer().frame(width: 20)
+                        
+                        VStack {
+                            Text("Current rate:")
+                                .frame(maxWidth: .infinity, alignment: .trailing)
+                                .foregroundColor(.gray)
+                                .italic()
+                                .padding(.top, 30)
+                            Text("\(exchangeRate, specifier: "%.2f")")
+                                .frame(maxWidth: .infinity, alignment: .trailing)
+                                .padding(.bottom, 30)
                         }
+                        
+                        Spacer().frame(width: 15)
+                        
+                        Divider()
+                            .frame(height: 50)
+                            .background(Color.gray)
+                        
+                        Spacer().frame(width: 20)
+                        
+//                            Image(systemName: "chevron.right")
+//                                .padding(.trailing, 10)
+//                                .foregroundColor(.black)
+                        
+                        Image(systemName: "chevron.right")
+                            .padding(.trailing, 10)
+                            .foregroundColor(.black)
+                            .overlay(
+                                NavigationLink(
+                                    destination: MoneyExchangeView(currencyCode: currencyCode, exchangeRate: exchangeRate),
+                                    label: { EmptyView() }
+                                )
+                                .opacity(0)
+                            )
+                        
+                        Spacer().frame(width: 1)
                     }
                 }
             }
-            .navigationBarTitle("Exchange Rates", displayMode: .inline)
-            .onAppear {
-                print("View appeared, calling fetchExchangeRates()")
-                viewModel.fetchExchangeRates()
-            }
-            .onReceive(viewModel.$exchangeRates) { _ in
-                // Update the UI on the main thread
-                DispatchQueue.main.async {
-                    // Force a view update by changing a @State variable
-                    // This will trigger a refresh of the view
-                }
+        }
+        .navigationBarTitle("Exchange Rates", displayMode: .inline)
+        .onAppear {
+            print("View appeared, calling fetchExchangeRates()")
+            viewModel.fetchExchangeRates()
+        }
+        .onReceive(viewModel.$exchangeRates) { _ in
+            // Update the UI on the main thread
+            DispatchQueue.main.async {
+                // Force a view update by changing a @State variable
+                // This will trigger a refresh of the view
             }
         }
     }
 }
+
 
 //struct CurrencyExchangeView_Previews: PreviewProvider {
 //    static var previews: some View {
