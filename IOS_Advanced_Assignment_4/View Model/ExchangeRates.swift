cece2138a2cef7cd9fb6ed9e260bea815b966ce6
solgrace <solibungrace@gmail.com>
2023-10-26 23:32:20 +1100

    Fixed 'assign' operator error by making exchangeRates non-optional

--- IOS_Advanced_Assignment_4/View Model/ExchangeRates.swift
+++ IOS_Advanced_Assignment_4/View Model/ExchangeRates.swift
@@ -9,7 +9,12 @@ import Foundation
 import Combine
 
 class ExchangeRatesViewModel: ObservableObject {
-    @Published var exchangeRates: ExchangeRatesResponse?
+    @Published var exchangeRates: ExchangeRatesResponse 
+
+    init() {
+        // Initialize exchangeRates with a default value or an empty ExchangeRatesResponse
+        self.exchangeRates = ExchangeRatesResponse(success: false, timestamp: 0, base: "", date: "", rates: [:])
+    }
 
     func fetchExchangeRates() {
         guard let url = URL(string: "http://data.fixer.io/api/latest?access_key=b1344cef01a490fd0b9562873c917841") else { return }
@@ -17,7 +22,6 @@ class ExchangeRatesViewModel: ObservableObject {
         URLSession.shared.dataTaskPublisher(for: url)
             .map(\.data)
             .decode(type: ExchangeRatesResponse.self, decoder: JSONDecoder())
-//            .replaceError(with: nil)
             .replaceError(with: ExchangeRatesResponse(success: false, timestamp: 0, base: "", date: "", rates: [:]))
             .receive(on: DispatchQueue.main)
             .assign(to: &$exchangeRates)
