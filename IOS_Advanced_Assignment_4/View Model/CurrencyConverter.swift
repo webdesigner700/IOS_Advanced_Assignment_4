8f0e4ac4fc0e29dcf677e58dc7ce529b4ce8ada0
solgrace <solibungrace@gmail.com>
2023-10-29 20:09:10 +1100

    Implemented CurrencyConverter View Model for currency conversion functionality

--- IOS_Advanced_Assignment_4/View Model/CurrencyConverter.swift
+++ IOS_Advanced_Assignment_4/View Model/CurrencyConverter.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+import SwiftUI
+
+class CurrencyConverterViewModel: ObservableObject {
+    @Published var inputAmount: String = ""
+    @Published var convertedAmount: Double = 0.0
+    let currencyCode: String
+    let exchangeRate: Double
+
+    init(currencyCode: String, exchangeRate: Double) {
+        self.currencyCode = currencyCode
+        self.exchangeRate = exchangeRate
+    }
+
+    func convertAmount() {
+        if let amount = Double(inputAmount) {
+            convertedAmount = amount * exchangeRate
+        }
+    }
+}
