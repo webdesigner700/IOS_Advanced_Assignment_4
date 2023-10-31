97bbdb634baa7440c3229207fd5878e3785c0e6d
solgrace <solibungrace@gmail.com>
2023-10-31 23:53:20 +1100

    Defined my Widgets data struct

---
+++ ExpensesWidget/WidgetDataModel.swift
@@ -0,0 +1,13 @@
+//
+//  WidgetDataModel.swift
+//  IOS_Advanced_Assignment_4
+//
+//  Created by Grace Rufina Solibun on 31/10/2023.
+//
+
+import Foundation
+
+struct WidgetExpenseData {
+    var category: String
+    var topThreeExpenses: [(name: String, amount: Int)]
+}
